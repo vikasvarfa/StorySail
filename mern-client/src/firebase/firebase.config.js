f7169a1a308854200a7d1e850335a78ead24d8fa
vikasvarfa <vikasvarfa98@gmail.com>
2024-01-18 15:16:45 +0530

    first commit

---
+++ mern-client/src/firebase/firebase.config.js
@@ -0,0 +1,45 @@
+/*
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCIQl0weHWkyX7FNmGNCTElUYL9aL0HPMA",
+    authDomain: "online-book-store-78516.firebaseapp.com",
+    projectId: "online-book-store-78516",
+    storageBucket: "online-book-store-78516.appspot.com",
+    messagingSenderId: "282181520275",
+    appId: "1:282181520275:web:0ce28538cea0433ea83666"
+  };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
+
+
+*/
+// my api key 
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA1Yrjj_Thl_5sMVuoYNp1YGQJDEHahNj4",
+  authDomain: "mern-book-inventory-811b8.firebaseapp.com",
+  projectId: "mern-book-inventory-811b8",
+  storageBucket: "mern-book-inventory-811b8.appspot.com",
+  messagingSenderId: "106703483479",
+  appId: "1:106703483479:web:1a7750c01d58d5669ea912"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
